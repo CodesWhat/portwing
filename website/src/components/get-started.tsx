ca1a21dc93f48025814e15c334e554acb72156fa
scttbnsn <80784472+scttbnsn@users.noreply.github.com>
2026-09-12 21:36:04 -0400

    chore(release): prepare v0.9.18 documentation and examples

--- website/src/components/get-started.tsx
+++ website/src/components/get-started.tsx
@@ -165,10 +165,10 @@ export function GetStarted() {
 brew install --cask codeswhat/tap/portwing
 
 # Debian / Ubuntu
-sudo apt install ./portwing_0.9.17_linux_amd64.deb
+sudo apt install ./portwing_0.9.18_linux_amd64.deb
 
 # Fedora / RHEL
-sudo rpm --install ./portwing_0.9.17_linux_amd64.rpm`}</code>
+sudo rpm --install ./portwing_0.9.18_linux_amd64.rpm`}</code>
               </pre>
             </div>
           )}
